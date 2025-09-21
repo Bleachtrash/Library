22f7104492111df129a69ac1096f721ac76bcf2a
Peter Gloag <petergloag@gmail.com>
2025-09-21 15:22:01 -0700

    Added static and non-static fields

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,17 @@
-public class Book {
+import java.time.LocalDate;
 
+public class Book {
+    public static  int ISNB_;
+    public static int TITLE_;
+    public static int SUBJECT_;
+    public static int Page_Count_;
+    public static int AUTHOR_;
+    public static int Due_Date_;
+
+    private String author;
+    private LocalDate dueDate;
+    private String isbn;
+    private int pageCount;
+    private String subject;
+    private String title;
 }

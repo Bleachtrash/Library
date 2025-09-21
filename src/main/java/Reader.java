158701a3aff5dc8040b551627903e7a306f8d95e
Peter Gloag <petergloag@gmail.com>
2025-09-21 15:49:17 -0700

    Added static and private fields

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,3 +1,14 @@
-public class Reader {
+import java.util.List;
 
+public class Reader {
+    public static int CARD_NUMBER_;
+    public static int NAME_;
+    public static int PHONE_;
+    public static int BOOK_COUNT_;
+    public static int BOOK_START_;
+
+    private int cardNumber;
+    private String name;
+    private String phone;
+    private List<Book> books;
 }

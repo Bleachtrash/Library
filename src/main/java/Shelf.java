a4b10921846336254916ec5a5b604bf72a577000
Peter Gloag <petergloag@gmail.com>
2025-09-28 18:07:40 -0700

    Added private fields

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,4 +1,11 @@
+import java.util.HashMap;
+
 public class Shelf {
     public static int SHELF_NUMBER_ = 0;
     public static int SUBJECT_ = 1;
+
+    private HashMap<Book, Integer> books;
+    private String subject;
+
+
 }

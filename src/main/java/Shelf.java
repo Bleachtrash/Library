06917574b22d84311f1af31fcb1cfd1a442e78dd
Peter Gloag <petergloag@gmail.com>
2025-09-28 18:11:31 -0700

    Added getters, setters, hasCode(), and equals()

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Shelf {
     public static int SHELF_NUMBER_ = 0;
@@ -8,4 +9,39 @@ public class Shelf {
     private int shelfNumber;
     private String subject;
 
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelf shelf = (Shelf) o;
+        return shelfNumber == shelf.shelfNumber && Objects.equals(subject, shelf.subject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shelfNumber, subject);
+    }
 }
